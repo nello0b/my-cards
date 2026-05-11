d430fbf8f050b9807dc1aaaaa61108aa7b9e986f
Netanel B. <nello0b@gmail.com>
2026-05-11 22:59:36 +0300

    Add Aquamiror Crash script with negate and ritual spell copy effects
    - Implemented a new Lua script for the Aquamiror card.
    - Added an effect to negate spell/trap activations and return a ritual monster to hand.
    - Included functionality to copy a ritual spell from hand or deck to graveyard.

---
+++ script/c11110020.lua
@@ -0,0 +1,71 @@
+--Aquamirror Crash
+local s,id=GetID()
+function s.initial_effect(c)
+	--return to negate and shuffle
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_NEGATE+CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_ACTIVATE)
+	e1:SetCondition(s.negcon)
+	e1:SetTarget(s.negtg)
+	e1:SetOperation(s.negop)
+	c:RegisterEffect(e1)
+	--copy ritual spell
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.cptg)
+	e2:SetOperation(s.cpop)
+	c:RegisterEffect(e2)
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL+TYPE_TRAP)
+end
+function s.negfilter(c)
+	return c:IsSetCard(0x3a) and c:IsType(TYPE_RITUAL) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand() and c:IsFaceup()
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local rc=re:GetHandler()
+	if chk==0 then return (not rc:IsRelateToEffect(re) or rc:IsAbleToDeck())
+		and Duel.IsExistingMatchingCard(s.negfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if rc:IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_TODECK,eg,1,0,0)
+	end
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local tc=Duel.SelectMatchingCard(tp,s.negfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	if tc and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_HAND)
+		and Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SendtoDeck(eg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	end
+end
+function s.cpfilter(c)
+	return c:GetType()==TYPE_SPELL+TYPE_RITUAL and c:IsAbleToGraveAsCost() and c:CheckActivateEffect(true,true,false)~=nil
+end
+function s.cptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cpfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil) end
+	e:SetLabel(1)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+end
+function s.cpop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==0 then return end
+	e:SetLabel(0)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.cpfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()==0 then return end
+	local te=g:GetFirst():CheckActivateEffect(true,true,false)
+	e:SetLabelObject(te)
+	Duel.SendtoGrave(g,REASON_COST)
+	e:SetProperty(te:GetProperty())
+	local tg=te:GetTarget()
+	if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
+	Duel.ClearOperationInfo(0)
+end
